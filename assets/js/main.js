d0c591f2534ff7477e428de10b433f2fde05af84
Vintageklopp <38953949+Vintageklopp@users.noreply.github.com>
2019-06-04 00:31:00 +0100

    Added initial mediworld website files

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+$('.open-menu-icon').on('click', function () {
+  $('.main-nav-outer').addClass('open-menu');
+});
+
+$('.close-menu-icon').click(function () {
+  $('.main-nav-outer').removeClass('open-menu');
+});
